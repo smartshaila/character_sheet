8d2508817fa6ff4e2e4621887389fc62fb5828b7
Icehawk78 <i.am@aweso.ms>
2015-04-16 13:16:37 -0400

    Remove test folder

--- test/helpers/spell_materials_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class SpellMaterialsHelperTest < ActionView::TestCase
-end
--- test/models/known_spell_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class KnownSpellTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
