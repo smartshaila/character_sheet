cf0ac6b285319839ee00c4ecbf5088c7a4f5d36b
smartshaila <smartshaila@gmail.com>
2015-04-23 08:37:57 -0400

    Fixed form loading data; Worked on form auto-save. Not working yet.

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -5,12 +5,24 @@
 
 atmlApp = angular
   .module('atmlApp',['ngMaterial'])
-  .controller('atmlCtrl', function ($scope, $http){
+  .controller('atmlCtrl', function ($scope, $http, $timeout){
     $scope.character = {
       current_xp: 0
     };
     $scope.levels = [];
     $scope.classes = [];
+
+    $scope.populate_classes = function (json) {
+      for (key in json) {
+        $scope.classes[parseInt(key)] = json[key];
+      }
+    };
+
+    $scope.character.populate = function (json) {
+      for (key in json) {
+        $scope.character[key] = json[key];
+      }
+    };
 
     $scope.character.level_progression = function () {
       return $scope.levels.sort(function (a, b) {
@@ -28,10 +40,26 @@ atmlApp = angular
       return $scope.character.level_progression().proficiency;
     };
 
+    var save_updates = function () {
+      $http.put('/characters/' + $scope.character.id, $scope.character);
+    };
+
+    var timeout = null;
+
+    var debounce_save_updates = function(newVal, oldVal) {
+      if (newVal != oldVal) {
+        if (timeout) {
+          $timeout.cancel(timeout)
+        }
+        timeout = $timeout(save_updates, 1000);  // 1000 = 1 second
+      }
+    };
+    $scope.$watch('character.name', debounce_save_updates);
+    $scope.$watch('character.current_xp', debounce_save_updates);
+
     $scope.init = function (id) {
-      $http.get('../classes.json').then(function (value) { $scope.classes = value.data; });
+      $http.get('../classes.json').then(function (value) { $scope.populate_classes(value.data); });
       $http.get('../levels.json').then(function (value) { $scope.levels = value.data; });
-      //Need to fix this; Overriding $scope.level etc
-      $http.get('../character_data/' + id + '.json').then(function (value) { $scope.character = value.data; });
+      $http.get('../character_data/' + id + '.json').then(function (value) { $scope.character.populate(value.data); });
     };
   });
